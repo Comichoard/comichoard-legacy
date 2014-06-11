c4c2cb4e823e523ab8bc5f626b7e40b4c3263a4e
znavfu <manish.shetty13@live.com>
2014-06-11 21:09:54 +0530

    This isnt gonna work

--- jl8/jl8.php
+++ jl8/jl8.php
@@ -1,80 +1,105 @@
 <?php
+    $mysql_hostname = "localhost";
+    $mysql_user = "comict";
+    $mysql_password = "comichoard";
+    $mysql_database = "comichoard";
+    $count = 0;
+    $url_present = "";
+    $next_id = "";
     $all = array();
 
-    function getfirst()     {
-        $url = 'http://jl8comic.tumblr.com';
-        $data = array('nothing' => 'blahblah');
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data),
-            ),
-        );
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
+    $con=mysqli_connect($mysql_hostname,$mysql_user,$mysql_password,$mysql_database);
 
-        preg_match('/jl8-(.*?)-/',$result, $matches);
-        return substr($matches[0],4,3);
+    function makeWell($src,$id,$next)
+    {
+        global $all;
+        array_push($all, '<div class="well">
+                <img alt="JL8 #'.$id.'" src="'.$src.'">
+                <div class="details">
+                    <span>#'.$id.'</span>'.'<span class="s btn btn-default btn-lg" data-share="'.base64_encode($id).'">Share</span>
+                </div>
+            </div>');
     }
 
-    $sort = 'desc';
-    function getcomic($num)   {
-        global $all,$sort;
-        if($num == 50)  {
-            $cnt = 1;
-            while($cnt != 9)    {
-                array_push($all, '<div class="well"><img alt="JL8 #'.$num.'" src="http://www.limbero.org/jl8/comics/'.$num.'_'.$cnt.'.jpeg"><div class="details"><span>#'.$num.'</span>'.'<span class="s btn btn-default btn-lg" data-share="'.base64_encode($num).'">Share</span></div></div>');    
-                $cnt++;
+    function dump50()   {
+        $result = mysqli_query($con,"SELECT * FROM jl8 where id LIKE '50%';");
+        while($row = mysqli_fetch_array($result))   {
+            $url_present = $row['url'];
+            $cur_id = $row['id'];
+            makeWell($url_present,$cur_id,49);
+        }
+    }
+    if (mysqli_connect_errno()) {
+        echo base64_encode('1').'!znavfu';
+        if(!isset($_GET['comic']))  {
+            echo '<div class="jumbotron cdesc"><h1>JL8 <a href="http://jl8comic.tumblr.com" type="button" class="btn btn-default" target="_blank">Go to site</a></h1>
+              <p>JL8 is a comic strip created by Yale Stewart. It tells tales based on younger versions of DC superheroes.</p></div>';
+            echo '<div class="jumbotron">There was an error while connecting to the library.<br>Here\'s something else to read <a href="http://comichoard.com/garfield" type="button" class="btn btn-default">Garfield</a></div>';
+        }
+    }
+    else
+    {
+        if(isset($_GET['comic'])){
+            $pres_id = base64_decode(($_GET['comic']));
+            if($pres_id == 50)  {
+                dump50();
+                $next_id = 49;
             }
+            else {
+                $result = mysqli_query($con,"SELECT * FROM jl8 where id = '".$pres_id."'");
+                while($row = mysqli_fetch_array($result))
+                    $url_present = $row['url'];
+                $next_id = $pres_id-1;       
+                makeWell($url_present,$pres_id,$next_id);
+            }
+            echo base64_encode($next_id).'!znavfu';
+            echo $all[0];
         }
         else {
-            array_push($all, '<div class="well"><img alt="JL8 #'.$num.'" src="http://www.limbero.org/jl8/comics/'.$num.'.jpeg"><div class="details"><span>#'.$num.'</span>'.'<span class="s btn btn-default btn-lg" data-share="'.base64_encode($num).'">Share</span></div></div>');
-        }
-        if($sort == 'desc')
-            return --$num;
-        else
-            return ++$num;
-    }
-
-    $last=getfirst();
-    $next=$last;
-    if(isset($_GET['comic'])) {
-        $recieved = explode('!sort',$_GET['comic']);
-        $sort = $recieved[1];
-        $num = base64_decode($recieved[0]);
-        if($num>$last || $num<1)
-            array_push($all,'<br><br><div class="jumbotron">Ran out of comics to show.<br>Here\'s something else to read <a href="http://comichoard.com/?comic=pcw" type="button" class="btn btn-default">PC Weenies</a> </div>');
-        else
-            $next = getcomic($num);
-        echo base64_encode($next).'!sort'.$sort.'!znavfu';
-        foreach($all as $item) echo $item;
-    }
-    else    {
-        if(isset($_GET['strip']))   {
-            getcomic(base64_decode($_GET['strip']));
-            array_push($all,'<div class="jumbotron">More comics from JL8...</div>');
-        }
-        else    {
-            if($_GET['sort'] == 'asc') {
-                $sort = 'asc';
-                $next = getcomic(1);
+            if(isset($_GET['strip']))   
+            {
+                $pres_id = base64_decode($_GET['strip']);
+                if($pres_id == 50)  {
+                    dump50();
+                    $next_id = 49;
+                }
+                else {
+                    $result = mysqli_query($con,"SELECT * FROM jl8 where id = '".$pres_id."'");
+                    while($row = mysqli_fetch_array($result))
+                        $url_present = $row['url'];
+                    $next_id = $pres_id-1;       
+                    makeWell($url_present,$pres_id,$next_id);
+                }
+                array_push($all,'<div class="jumbotron">More comics from JL8...</div>');
             }
-            else    {      
-                $sort = 'desc';
-                $next = getcomic($last);
+            else
+            {
+                $result = mysqli_query($con,"SELECT * FROM jl8 order by id desc");
+                while($row = mysqli_fetch_array($result)) {
+                    if($count==0)   {
+                        $url_present = $row['url'];
+                        $present_id = $row['id'];
+                    }
+                    if($count==1){
+                        $next_id = $row['id'];
+                        break;
+                    }
+                    $count += 1;
+                }
+                makeWell($url_present,$present_id,$next_id);
             }
-        }
-        echo base64_encode($next).'!sort'.$sort.'!znavfu';
-        echo '<div class="jumbotron cdesc"><h1>JL8 <a href="http://jl8comic.tumblr.com" type="button" class="btn btn-default" target="_blank">Go to site</a></h1>
+            echo base64_encode($next_id).'!znavfu';
+            echo '<div class="jumbotron cdesc"><h1>JL8 <a href="http://jl8comic.tumblr.com" type="button" class="btn btn-default" target="_blank">Go to site</a></h1>
               <p>JL8 is a comic strip created by Yale Stewart. It tells tales based on younger versions of DC superheroes.</p>
-              <p>
+              <!--p>
               <span>Sort in order
-              <a href="http://'.$_SERVER['HTTP_HOST'].'/jl8/?sort=asc" type="button" class="btn btn-default">From the start</a>
-              <a href="http://'.$_SERVER['HTTP_HOST'].'/jl8/?sort=desc" type="button" class="btn btn-default">Most recent first</a></span>
-              <span>Skip to comic # <input id="comicnumselect" type="text" class="form-control" placeholder="1-'.$last.'")"></span>
-              </p>
+              <a href="http://comichoard.com/jl8/?sort=asc" type="button" class="btn btn-default">From the start</a>
+              <a href="http://comichoard.com/jl8/?sort=desc" type="button" class="btn btn-default">Most recent first</a></span>
+              <span>Skip to comic # <input id="comicnumselect" type="text" class="form-control" placeholder=""></span>
+              </p-->
               </div>';
-        foreach($all as $item) echo $item;
-    }
+            foreach($all as $item) echo $item;
+        }
+        mysqli_close($con);
+    }   
 ?>
