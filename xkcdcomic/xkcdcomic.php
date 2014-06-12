d9aec0f931129967ba5a33f9a0fbfffced89879a
znavfu <manish.shetty13@live.com>
2014-06-12 23:34:06 +0530

    added new xkcdcomic

--- xkcdcomic/xkcdcomic.php
+++ xkcdcomic/xkcdcomic.php
@@ -1,67 +1,116 @@
 <?php
+    $mysql_hostname = "localhost";
+    $mysql_user = "root";
+    $mysql_password = "";
+    $mysql_database = "comichoard";
+    $count = 0;
+    $html_present = "";
+    $next_id = "";
     $all = array();
-    $source = 'xkcd';
 
-    function getfirst() {
-        $url = 'http://www.xkcd.com';
-        $data = array('nothing' => 'blahblah');
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data),
-            ),
-        );
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
+    $con=mysqli_connect($mysql_hostname,$mysql_user,$mysql_password,$mysql_database);
 
-        $first1 = explode('http://xkcd.com/', $result);
-        $first2 =  explode('/', $first1[1]);
-
-        return intval($first2[0]);
-    }
-
-    function getcomic($i)   {
+    function makeWell($html,$id,$next)
+    {
         global $all;
-        $url = 'http://www.xkcd.com/'.$i.'/';
-        $data = array('nothing' => 'blahblah');
-        $options = array(
-            'http' => array(
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data),
-            ),
-        );
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        $namebig = explode('<div id="ctitle">',$result);
-        $name = explode('</div>',$namebig[1]);
-        $first = explode('"comic">', $result);
-        $second = explode('</div>', $first[1]);
-        preg_match('/alt="(.*?)"/',$second[0], $alttoreplace);
-        $second[0] = str_replace($alttoreplace[0], 'alt="XKCD #'.$i.'"', $second[0]);
-        array_push($all, '<div class="well">'.$second[0].'<br><div class="details"><span>#'.$i.'</span><span>'.$name[0].'</span><span class="s btn btn-default btn-lg" data-share="'.$i.'">Share</span></div></div>');
-        return $i-1;
+        array_push($all, '<div class="well">'.$html.'
+                <div class="details">
+                    <span>#'.$id.'</span>'.'<span class="s btn btn-default btn-lg" data-share="'.base64_encode($id).'">Share</span>
+                </div>
+            </div>');
     }
 
-    if(isset($_GET['comic'])) {
-        $sendback = getcomic(base64_decode($_GET['comic']));
-        echo base64_encode($sendback).'!znavfu'.$all[0];
-    }
-    else    {
-        $count = 1;
-        if(isset($_GET['strip']))   {
-            getcomic($_GET['strip']);
-            array_push($all,'<div class="jumbotron">More comics from XKCD...</div>');
-            $count--;
+    if (mysqli_connect_errno()) {
+        echo base64_encode('1').'!znavfu';
+        if(!isset($_GET['comic']))  {
+            echo mysqli_connect_errno();
+            echo '<div class="jumbotron cdesc"><h1>XKCD <a href="http://xkcd.com" type="button" class="btn btn-default" target="_blank">Go to site</a></h1>
+              <p>XKCD, is a webcomic created by Randall Munroe.<br>The comic\'s tagline describes it as "a webcomic of romance, sarcasm, math, and language."</p></div>';
+            echo '<div class="jumbotron">There was an error while connecting to the library.<br>Here\'s something else to read <a href="http://comichoard.com/garfield" type="button" class="btn btn-default">Garfield</a></div>';
         }
-        $begin = getfirst();
-        $i = getcomic($begin);
-        echo base64_encode($i).'!znavfu';
-        echo '<div class="jumbotron cdesc"><h1>XKCD <a href="http://xkcd.com" type="button" class="btn btn-default" target="_blank">Go to site</a></h1>
-              <p>XKCD, is a webcomic created by Randall Munroe.<br>The comic\'s tagline describes it as "a webcomic of romance, sarcasm, math, and language."</p>
-              <p>Skip to comic # <input id="comicnumselect" type="text" class="form-control" placeholder="1-'.$begin.'")"></p>
-              </div>';
-        foreach($all as $item) echo $item;
     }
+    else
+    {
+        $result = mysqli_query($con,"SELECT MAX(id) as 'max' FROM xkcd;");
+        $row = mysqli_fetch_array($result);
+        $last = $row['max'];
+        if(isset($_GET['comic']))   {
+            $sort = 'desc';
+            if(isset($_GET['sort'])) {
+                if($_GET['sort'] == 'asc')  {
+                    $sort='asc';
+                }
+            }
+            $pres_id = base64_decode(($_GET['comic']));
+            if($pres_id == 0 || $pres_id == $last+1)
+                exit();
+            else {
+                $result = mysqli_query($con,"SELECT * FROM xkcd where id = '".$pres_id."'");
+                while (!isset($result)) {
+                    if($sort=='desc')
+                        $pres_id-=1;
+                    else
+                        $pres_id+=1;       
+                    $result = mysqli_query($con,"SELECT * FROM xkcd where id = '".$pres_id."'");
+                }
+                while($row = mysqli_fetch_array($result))
+                    $html_present = $row['html'];
+                if($sort=='desc')
+                    $next_id = $pres_id-1;       
+                else
+                    $next_id = $pres_id+1;   
+                makeWell($html_present,$pres_id,$next_id);
+                if($pres_id == 1 || $pres_id == $last)
+                    array_push($all,'<div class="jumbotron">Looks like you read all the comics we had.<br>Here\'s something else to read <a href="http://comichoard.com/garfield" type="button" class="btn btn-default">Garfield</a></div>');    
+            }
+            echo base64_encode($next_id).'!znavfu';
+            foreach($all as $item) echo $item;
+        }
+        else {
+            if(isset($_GET['strip']))   
+            {
+                $pres_id = base64_decode($_GET['strip']);
+                $result = mysqli_query($con,"SELECT * FROM xkcd where id = '".$pres_id."'");
+                while($row = mysqli_fetch_array($result))
+                    $html_present = $row['html'];
+                $next_id = $pres_id-1;       
+                makeWell($html_present,$pres_id,$next_id);
+                array_push($all,'<div class="jumbotron">More comics from XKCD...</div>');
+            }
+            else
+            {
+                $sort = 'desc';
+                if(isset($_GET['sort'])) {
+                    if($_GET['sort'] == 'asc')  {
+                        $sort='asc';
+                    }
+                }
+                $result = mysqli_query($con,"SELECT * FROM xkcd order by id ".$sort.";");
+                while($row = mysqli_fetch_array($result)) {
+                    if($count==0)   {
+                        $html_present = $row['html'];
+                        $present_id = $row['id'];
+                    }
+                    if($count==1){
+                        $next_id = $row['id'];
+                        break;
+                    }
+                    $count += 1;
+                }
+                makeWell($html_present,$present_id,$next_id);
+            }
+            echo base64_encode($next_id).'!znavfu';
+            echo '<div class="jumbotron cdesc"><h1>XKCD <a href="http://xkcd.com" type="button" class="btn btn-default" target="_blank">Go to site</a></h1>
+              <p>XKCD, is a webcomic created by Randall Munroe.<br>The comic\'s tagline describes it as "a webcomic of romance, sarcasm, math, and language."</p>
+              <p>
+              <span>Sort in order
+              <a href="http://comichoard.com/xkcdcomic/?sort=asc" type="button" class="btn btn-default">From the start</a>
+              <a href="http://comichoard.com/xkcdcomic/?sort=desc" type="button" class="btn btn-default">Most recent first</a></span>
+              <span>Skip to comic # <input id="comicnumselect" type="text" class="form-control" placeholder="1-'.$last.'"></span>
+              </p>
+              </div>';
+            foreach($all as $item) echo $item;
+        }
+        mysqli_close($con);
+    }   
 ?>
