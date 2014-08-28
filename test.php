45a1029646568d37ac067c623ec634bb2a6006f3
znavfu <manish.shetty13@live.com>
2014-08-28 15:31:00 +0530

    check out mobile, (y)

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$ch = curl_init('http://tt14online.techtatva.in/api/?username=atish&password=sharma&event=civilian');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($ch);
-    echo $result;
-?>
